51f653583173c71917a4d873c0b6d9caa1c582d5
Leonardo Alves <leonardo.af7@gmail.com>
2022-09-21 19:00:35 -0300

    Add script to evaluate the results (OCR/SSIM/PSNR)

---
+++ evaluate.py
@@ -0,0 +1,104 @@
+import argparse
+import os
+import numpy as np
+from utils import evaluation_tools
+
+# Get input arguments
+parser = argparse.ArgumentParser(description=
+                                    'Calculate evaluation metrics (PSNR/SSIM/OCR) \
+                                        for the deblurred images.'
+                                )
+
+parser.add_argument('--deblurred_path', type=str,
+                        help='Path with the results of the deblurring algorithm.'
+                    )
+parser.add_argument('--text_path', type=str, default=None, required=False,
+                        help='Path with text files containing the ground truth transcription\
+                            of the images (must have the same name of the corresponding deblurred image).'
+                    )
+parser.add_argument('--groundTruth_path', type=str, default=None, required=False,
+                        help='Path with the ground truth images (must have the same name of the\
+                        corresponding deblurred image, but can be of different file extension).'
+                    )   
+parser.add_argument('--calculate_OCR', action='store_true',
+                        help='Calculate the OCR metric for the images. Require path to \
+                            text files (--text_path).'
+                    )
+parser.add_argument('--calculate_PSNR', action='store_true',
+                        help='Calculate the PSNR metric for the images. Require path to \
+                            ground truth images (--groundTruth_path).'
+                    )
+parser.add_argument('--calculate_SSIM', action='store_true',
+                        help='Calculate the SSIM metric for the images. Require path to \
+                            ground truth images (--groundTruth_path).'
+                    )
+parser.add_argument('--omit_progress', action='store_false',
+                        help='Omit the print of the execution progress\
+                            (current image number/total image number).'
+                    )
+parser.add_argument('--save_name', type=str, default=None, required=False,
+                        help='Name (including desired path) to save the dictionary with the metrics results.'
+                    )                                             
+                                     
+args = parser.parse_args()
+
+# Get image names
+deblurred_names = sorted(os.listdir(args.deblurred_path))
+print(f"{len(deblurred_names)} images were found.")
+
+results = {}
+
+assert(args.calculate_OCR or args.calculate_PSNR or args.calculate_SSIM), "No metric was specified. Use the options\
+    --calculate_OCR, --calculate_PSNR and/or --calculate_SSIM to specify the metrics to calculate."
+
+if(args.calculate_PSNR or args.calculate_SSIM):
+    assert(args.groundTruth_path is not None), "Calculating the PSNR/SSIM score requires\
+                                                 the groundTruth_path argument."
+
+    groundTruth_names = sorted(os.listdir(args.groundTruth_path))
+
+    assert (len(deblurred_names) == len(groundTruth_names)), "Ground truth and deblurred folders have\
+                                                            a different number of images."
+    if(args.calculate_PSNR):
+        results['psnr'] = []
+
+    if(args.calculate_SSIM):
+        results['ssim'] = []
+
+if(args.calculate_OCR):
+    assert(args.text_path is not None), "Calculating the OCR score requires the text_path argument."
+
+    text_names = sorted(os.listdir(args.text_path))
+    assert (len(deblurred_names) == len(text_names)), "Text and deblurred folders have\
+                                                        a different number of files."
+    results['ocr'] = []
+
+for i_img in range(len(deblurred_names)):
+    deblurredFile = os.path.join(args.deblurred_path, deblurred_names[i_img])
+
+    if(args.calculate_OCR):
+        textFile = os.path.join(args.deblurred_path, text_names[i_img])
+        OCR_score = evaluation_tools.evaluateImage_OCR(deblurredFile, textFile)
+        results['ocr'].append(OCR_score)
+    
+    if(args.calculate_PSNR or args.calculate_SSIM):
+        referenceFIle = os.path.join(args.groundTruth_path, groundTruth_names[i_img])
+
+        PSNR_score, SSIM_score = evaluation_tools.evaluateImage_Quality(deblurredFile, referenceFIle,
+                                                                        args.calculate_PSNR, args.calculate_SSIM)
+        if(args.calculate_PSNR):
+            results['psnr'].append(PSNR_score)
+        
+        if(args.calculate_SSIM):
+            results['ssim'].append(SSIM_score)
+    
+    if(not args.omit_progress):
+        print(f"{i_img}/{len(deblurred_names)}")
+
+for metric in results.keys():
+    results[metric+'_mean'] = np.mean(results[metric])
+    results[metric+'_std'] = np.std(results[metric])
+    print(f"{metric.upper()} result: {results[metric+'_mean']} +- {results[metric+'_std']}")
+
+if(args.save_path is not None):
+    np.save(args.save_path, results, allow_pickle=True)
---
+++ utils/evaluation_tools.py
@@ -0,0 +1,103 @@
+import numpy as np
+from PIL import Image
+import pytesseract
+from fuzzywuzzy import fuzz
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from utils import autoencoder_tools
+
+def normalize(img):
+    """
+    Linear histogram normalization
+    source: https://fips.fi/HDC2021_ocrscorecode.zip
+    """
+    arr = np.array(img, dtype=float)
+
+    arr = (arr - arr.min()) * (255 / arr[:, :50].min())
+    arr[arr > 255] = 255
+    arr[arr < 0] = 0
+
+    return Image.fromarray(arr.astype('uint8'), 'L')
+
+def evaluateImage_OCR(imageFile, trueTextFile, norm=True):
+    """
+    OCR evaluation
+    source: https://fips.fi/HDC2021_ocrscorecode.zip
+    """
+    with open(trueTextFile, 'r') as f:
+        trueText = f.readlines()
+
+    # remove \n character
+    trueText = [text.rstrip() for text in trueText]
+
+    # load image and convert to grayscale
+    img = Image.open(imageFile)
+    w, h = img.size
+
+    if (norm):
+      img = normalize(img)
+
+    # resize image to improve OCR
+    img = img.resize((int(w / 2), int(h / 2)))
+    w, h = img.size
+
+    # run OCR
+    options = r'--oem 1 --psm 6 -c load_system_dawg=false -c load_freq_dawg=false  -c textord_old_xheight=0  -c textord_min_xheight=100 -c ' \
+              r'preserve_interword_spaces=0'
+    OCRtext = pytesseract.image_to_string(img, config=options)
+
+    # removes form feed character  \f
+    OCRtext = OCRtext.replace('\n\f', '').replace('\n\n', '\n')
+
+    # split lines
+    OCRtext = OCRtext.split('\n')
+
+    # remove empty lines
+    OCRtext = [x.strip() for x in OCRtext if x.strip()]
+
+    # check if OCR extracted 3 lines of text
+    #print('File:' + imageFile)
+    #print('True text (middle line): %s' % trueText[1])
+
+    if len(OCRtext) != 3:
+        #print('ERROR: OCR text does not have 3 lines of text!')
+        #print(OCRtext)
+        return None
+    else:
+        score = fuzz.ratio(trueText[1], OCRtext[1])
+        #print('OCR  text (middle line): %s' % OCRtext[1])
+        #print('Score: %d' % score)
+
+        return float(score)
+
+def normalize_maxmin(img):
+    """
+    Normalization applied before calculating PSNR and SSIM metrics
+    """
+    arr = np.array(img, dtype=float)
+
+    arr = (arr - arr.min()) / (arr.max()-arr.min())*255
+
+    return arr.astype('uint8')
+
+def evaluateImage_Quality(imgFile, refFile, calc_PSNR, calc_SSIM):
+    """
+    PSNR and SSIM evaluation
+    """
+    ref = normalize_maxmin(Image.open(refFile))
+    img = normalize_maxmin(Image.open(imgFile))
+    
+    T = autoencoder_tools.get_transform(img,ref)
+    ref2 = autoencoder_tools.apply_transform(ref,T)
+    ref2 = normalize_maxmin(ref2)
+    
+    PSNR_score = None
+    SSIM_score = None
+    
+    if(calc_PSNR):
+        PSNR_score = psnr(ref2, img, data_range=255)
+
+    if(calc_SSIM):
+        SSIM_score = ssim(ref2, img, data_range=255)
+
+    return PSNR_score, SSIM_score
